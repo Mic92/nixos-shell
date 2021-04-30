c57d8deacaeecfd30180377a7ad66a96241b1c28
Jörg Thalheim <joerg@thalheim.io>
2021-04-30 07:49:09 +0200

    default.nix: ignore some files/dirs when importing source
    fixes https://github.com/Mic92/nixos-shell/issues/35

--- default.nix
+++ default.nix
@@ -1,7 +1,12 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "nixos-shell";
-  src = ./.;
+  src = builtins.filterSource
+    (path: type: baseNameOf path != "nixos.qcow2" &&
+                 baseNameOf path != ".git" &&
+                 baseNameOf path != ".direnv" &&
+                 baseNameOf path != "result"
+    ) ./.;
   buildInputs = [ bash ];
   preConfigure = ''
     export PREFIX=$out
