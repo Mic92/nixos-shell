c18c9652733db9a8f5ac939c74f26167f512f93b
Han Verstraete <welteki@pm.me>
2021-12-14 20:26:45 +0000

    add support for flake modules

--- share/nixos-shell.nix
+++ share/nixos-shell.nix
@@ -6,31 +6,49 @@
 , flakeAttr ? null
 }:
 let
+  lib = (import nixpkgs { }).lib;
+
   nixos-shell = import ./modules/nixos-shell.nix;
   nixos-shell-config = import ./modules/nixos-shell-config.nix;
 
-  flake = builtins.getFlake flakeUri;
-  flakeSystem = flake.outputs.packages."${system}".nixosConfigurations."${flakeAttr}" or flake.outputs.nixosConfigurations."${flakeAttr}";
-in
-if flakeUri != null then
-  flakeSystem.override
-    (attrs: {
-      modules =
-        let
-          nixosShellModules =
-            if flakeSystem ? options.nixos-shell then
-              [ nixos-shell-config ]
-            else
-              [ nixos-shell nixos-shell-config ];
-        in
-        attrs.modules ++ nixosShellModules;
-    })
-else
-  import "${toString nixpkgs}/nixos/lib/eval-config.nix" {
+  defaultTo = default: e: if e == null then default else e;
+
+  getFlakeOutput = path: lib.attrByPath path null flake.outputs;
+
+  mkShellSystem = config: import "${toString nixpkgs}/nixos/lib/eval-config.nix" {
     inherit system;
     modules = [
-      configuration
+      config
       nixos-shell
       nixos-shell-config
     ];
-  }
+  };
+
+  flake = builtins.getFlake flakeUri;
+
+  flakeSystem = defaultTo
+    (getFlakeOutput [ "nixosConfigurations" "${flakeAttr}" ])
+    (getFlakeOutput [ "packages" "${system}" "nixosConfigurations" "${flakeAttr}" ]);
+
+  flakeModule = getFlakeOutput [ "nixosModules" "${flakeAttr}" ];
+in
+if flakeUri != null then
+  if flakeSystem != null then
+    flakeSystem.override
+      (attrs: {
+        modules =
+          let
+            nixosShellModules =
+              if flakeSystem ? options.nixos-shell then
+                [ nixos-shell-config ]
+              else
+                [ nixos-shell nixos-shell-config ];
+          in
+          attrs.modules ++ nixosShellModules;
+      })
+  else if flakeModule != null then
+    mkShellSystem flakeModule
+  else
+    throw "cannot find flake attribute '${flakeUri}#${flakeAttr}'"
+else
+  mkShellSystem configuration
