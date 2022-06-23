4f3df0de1401ed6503da3586da8eff1b854d4b51
Matt Schreiber <schreibah@gmail.com>
2022-06-23 08:54:12 -0400

    print error when given non-extensible config
    that describes the issue (inability to mix in nixos-shell's modules) and
    links to the README

--- share/nixos-shell.nix
+++ share/nixos-shell.nix
@@ -2,6 +2,7 @@
 , system ? builtins.currentSystem
 , configuration ? <nixos-config>
 
+, flakeStr ? null # flake as named on the command line
 , flakeUri ? null
 , flakeAttr ? null
 }:
@@ -42,8 +43,28 @@ if flakeUri != null then
   if flakeSystem != null then
     if flakeSystem ? "extendModules" then
       flakeSystem.extendModules { modules = nixosShellModules; }
-    else
+    else if flakeSystem ? "override" then
       flakeSystem.override (attrs: { modules = attrs.modules ++ nixosShellModules; })
+    else
+      throw ''
+          '${flakeStr}#${flakeAttr}' is missing the expected 'override' attribute.
+
+          Please ensure that '${flakeStr}#${flakeAttr}' is an overridable attribute set by declaring it with 'lib.makeOverridable'.
+
+          For instance:
+
+              nixosConfigurations = let
+                lib = nixpkgs.lib;
+              in {
+                "${flakeAttr}" = lib.makeOverridable lib.nixosSystem {
+                  # ...
+                };
+              };
+
+          Alternatively, upgrade to a version of nixpkgs that provides the 'extendModules' function on NixOS system configurations.
+
+          See https://github.com/Mic92/nixos-shell#start-a-virtual-machine for additional information.
+      ''
   else if flakeModule != null then
     mkShellSystem flakeModule
   else
