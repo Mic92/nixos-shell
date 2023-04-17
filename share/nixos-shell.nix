Mic92/nixos-shell

commit 3e3dd034c8e2727d533915181222f4f31d37d201

Use flake.inputs.nixpkgs.lib if available, before defaulting to NIX_PATH
If NIX_PATH is unset, nixos-shell would crash, even if flake inputs were available in the target nixosConfiguration, this commit fixes that by defaulting lib to the target flake's nixpkgs input

share/nixos-shell.nix
@@ -6,7 +6,7 @@
 , flakeAttr ? null
 }:
 let
-  lib = (import nixpkgs { }).lib;
+  lib = flake.inputs.nixpkgs.lib or (import nixpkgs { }).lib;
 
   nixos-shell = import ./modules/nixos-shell.nix;
   nixos-shell-config = import ./modules/nixos-shell-config.nix;
@@ -15,7 +15,7 @@ let
 
   getFlakeOutput = path: lib.attrByPath path null flake.outputs;
 
-  mkShellSystem = config: import "${toString nixpkgs}/nixos/lib/eval-config.nix" {
+  mkShellSystem = config: import "${toString flake.inputs.nixpkgs or nixpkgs}/nixos/lib/eval-config.nix" {
     inherit system;
     modules = [
       config

commit 55d092c9f0d043c09d5c92297995c623676f1761

Fix bug introduced by 3e3dd034c8e2727d533915181222f4f31d37d201, which broke the non-flake use case

share/nixos-shell.nix
@@ -15,7 +15,7 @@ let
 
   getFlakeOutput = path: lib.attrByPath path null flake.outputs;
 
-  mkShellSystem = config: import "${toString flake.inputs.nixpkgs or nixpkgs}/nixos/lib/eval-config.nix" {
+  mkShellSystem = config: import "${if !(flakeUri == null) then (toString flake.inputs.nixpkgs) else (toString nixpkgs)}/nixos/lib/eval-config.nix" {
     inherit system;
     modules = [
       config